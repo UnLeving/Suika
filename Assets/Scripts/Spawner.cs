78650dfbe85349197131a813516da76b6d768fb0
ALeving <fgileving@gmail.com>
2025-08-01 17:56:45 +0300

    other files

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,9 +1,22 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] fruitsPrefabs;
+    public static Spawner Instance { get; private set; }
+         
+    
+    
+    [SerializeField] private Fruit[] fruitsPrefabs;
     [SerializeField] private int spawnIndex = -1;
+
+    private int indToSpawn;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     public void OnAttack()
     {
@@ -14,13 +27,24 @@ public class Spawner : MonoBehaviour
     {
         if (spawnIndex != -1)
         {
-            Instantiate(fruitsPrefabs[spawnIndex], transform.position, Quaternion.identity);
+            indToSpawn = spawnIndex;
         }
         else
         {
             var randIndex = Random.Range(0, fruitsPrefabs.Length);
             
-            Instantiate(fruitsPrefabs[randIndex], transform.position, Quaternion.identity);
+            indToSpawn = randIndex;
         }
+        
+        Fruit fruit = Instantiate(fruitsPrefabs[indToSpawn], transform.position, Quaternion.identity);
+            
+        //fruit.ind = indToSpawn;
+    }
+
+    public void RequestToMerge(int ind, Vector2 pos)
+    {
+        Fruit fruit = Instantiate(fruitsPrefabs[++ind], pos, Quaternion.identity);
+            
+        //fruit.ind = indToSpawn;
     }
 }
