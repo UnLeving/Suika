e7ec7b21917a95d1e0d7c7a0a8ed2fe5230548ee
ALeving <fgileving@gmail.com>
2025-09-10 23:47:47 +0300

    pdate build sctipt

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -16,4 +16,59 @@ public static class BuildScript
             BuildOptions.None
         );
     }
+    
+    public static void PerformWindowsBuild()
+    {
+        string path = "Builds/Windows";
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+        
+        BuildPipeline.BuildPlayer(
+            new[] { "Assets/Scenes/SampleScene.unity" },
+            path,
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None
+        );
+    }
+
+    public static void PerformAndroidBuildDevelopment()
+    {
+        string dir = "Builds/Android/Development";
+        if (!Directory.Exists(dir))
+            Directory.CreateDirectory(dir);
+
+        // Build APK for development (Debug)
+        EditorUserBuildSettings.buildAppBundle = false;
+        EditorUserBuildSettings.androidBuildType = AndroidBuildType.Debug;
+
+        string apkPath = Path.Combine(dir, "App-Dev.apk");
+
+        BuildPipeline.BuildPlayer(
+            new[] { "Assets/Scenes/SampleScene.unity" },
+            apkPath,
+            BuildTarget.Android,
+            BuildOptions.Development | BuildOptions.AllowDebugging
+        );
+    }
+
+    // todo: signing
+    public static void PerformAndroidBuildRelease()
+    {
+        string dir = "Builds/Android/Release";
+        if (!Directory.Exists(dir))
+            Directory.CreateDirectory(dir);
+
+        // Build AAB for release (Release)
+        EditorUserBuildSettings.buildAppBundle = true;
+        EditorUserBuildSettings.androidBuildType = AndroidBuildType.Release;
+
+        string aabPath = Path.Combine(dir, "App-Release.aab");
+
+        BuildPipeline.BuildPlayer(
+            new[] { "Assets/Scenes/SampleScene.unity" },
+            aabPath,
+            BuildTarget.Android,
+            BuildOptions.None
+        );
+    }
 }
